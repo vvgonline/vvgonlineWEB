4a8cc90a40a293102423891e341ba1f625023bde
vvgonline <vvgonline.net@gmail.com>
2023-12-04 21:53:16 +0530

    restructure web 04 dec 2023

--- wwwroot/TypeScript/src/Greet.ts
+++ wwwroot/TypeScript/src/Greet.ts
@@ -1,3 +1,7 @@
 export function sayHello(name: string) {
     return `Hello from ${name}`;
 }
+
+// export function sayHello(name: string) {
+//     return `Hello from ${name}`;
+// }
--- wwwroot/TypeScript/src/site.js
+++ wwwroot/TypeScript/src/site.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const greet_1 = require("./greet");
-console.log(greet_1.sayHello('TypeScript'));
+console.log((0, greet_1.sayHello)('TypeScript'));
