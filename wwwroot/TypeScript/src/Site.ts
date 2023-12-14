f03f8fd03eff0b14ecf39f29af791e5aee3789f9
vvgonline <vvgonline.net@gmail.com>
2023-12-14 19:43:21 +0530

    contact form embed done 14 dec 2023

--- wwwroot/TypeScript/src/Site.ts
+++ wwwroot/TypeScript/src/Site.ts
@@ -12,7 +12,7 @@
 //Module Test Hello World App
 import { sayHello } from './greet';
 
-console.log(sayHello('vvg online') + Date.toString());
+console.log(sayHello('vvg online'));
 //Status: Working
 
 // /**
